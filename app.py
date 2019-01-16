635d7673dbfbbe26f562ff8ddaa29c0418fb2a47
NIZAR SELMANI <A472583@arpege.socgen>
2019-01-16 15:37:34 +0100

    Fixed issue for create_table() in app.py file

--- app.py
+++ app.py
@@ -14,36 +14,6 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    """
-    class ItemModel(db.Model):
-        __tablename__ = 'items'
-
-        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-        name = db.Column(db.String(80))
-        price = db.Column(db.Float(precision=2))
-
-        store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
-        store = db.relationship('StoreModel')
-
-    class StoreModel(db.Model):
-        __tablename__ = 'stores'
-
-        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-        name = db.Column(db.String(80))
-
-        items = db.relationship('ItemModel', lazy='dynamic')
-
-    class UserModel(db.Model):
-        __tablename__ = 'users'
-
-        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-        username = db.Column(db.String(80))
-        password = db.Column(db.String(80))
-    """
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Store, '/store/<string:name>')
